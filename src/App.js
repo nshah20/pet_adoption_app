329a6b841025c400c21c3c01998d990d49c490ef
Neel Shah <neel.shah8122@gmail.com>
2021-04-27 15:19:05 -0500

    added theme and modal

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import { render } from "react-dom";
 import Pet from "./Pet.jsx";
 import SearchParams from './SearchParams';
 import {Router, Link} from '@reach/router';
 import Details from './Details';
+import ThemeContext from './ThemeContext';
 const App = () => {
+    const themeHook = useState('darkblue');
     return (
-        <div>
-            <header>
-                <Link to="/">
-                    Adopt Me test
-                </Link>
-            </header>
-            <Router>
-                <SearchParams path="/"/>
-                <Details path="/details/:id"/>
-            </Router>
-        </div>
+        <ThemeContext.Provider value={themeHook}>
+            <div>
+                <header>
+                    <Link to="/">
+                        Adopt Me test
+                    </Link>
+                </header>
+                <Router>
+                    <SearchParams path="/"/>
+                    <Details path="/details/:id"/>
+                </Router>
+            </div>
+        </ThemeContext.Provider>
     )
 };
 
